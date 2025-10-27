a28bcfc52f5de65a9355e4309c9ad502de1b0a6a
ercjer <jerceg01@fesb.hr>
2025-10-27 09:23:20 +0100

    v2, v3

--- vjezba2i3.c
+++ vjezba2i3.c
@@ -20,11 +20,16 @@ int addStart(Position prev);
 int printList(Position p);
 int addEnd(Position p);
 Position createNode(Position p);
-int checkAllocation(Position p);
 int searchList(Position p);
 int getAction(int actionOrSearch);
 int clearMemory(Position p);
 int deleteElement(Position p);
+int addElementAfter(Position p);
+int comparePersons(Position p, char name[16], char lastName[16], int year);
+int addElementBefore(Position p);
+int writeListInFile(Position p);
+int readListFromFile(Position p);
+int addSorted(Position p, char name[16], char lastName[16], int year);
 
 int main()
 {
@@ -32,8 +37,6 @@ int main()
 	head.next = NULL;
 	int action = -1;
 	int isRunning = 1;
-	//int searchOption = 0;
-	//int searching = 1;
 
 
 	printf("Odaberite akciju unosom broja:\n"
@@ -42,7 +45,11 @@ int main()
 			"3 - Dodavanje osobe na kraj liste.\n"
 			"4 - Trazenje osobe po imenu, prezimenu ili godini.\n"
 			"5 - Brisanje odredene osobe.\n"
-			"6 - Izlaz iz programa.\n\n");
+			"6 - Dodavanje osobe iza odredene.\n"
+			"7 - Dodavanje osobe ispred odredene.\n"
+			"8 - Upisuje listu u datoteku.\n"
+			"9 - Sortirano po prezimenu, ucitava listu iz datoteke.\n"
+			"10 - Izlaz iz programa.\n\n");
 			
 	while (isRunning) {
 		action = getAction(0);
@@ -63,14 +70,32 @@ int main()
 			}
 			break;
 		case 4:
-			//printf("Izaberi po cemu zelis traziti:\n1 - Po imenu.\n2 - Po prezimenu.\n3 - Po godini.\n");
-			//scanf(" %d", &searchOption);
 			searchList(&head);
 			break;
 		case 5: 
 			deleteElement(&head);
 			break;
 		case 6: 
+			if (addElementAfter(&head) < 0) {
+				return -1;
+			}
+			break;
+		case 7:
+			if (addElementBefore(&head) < 0) {
+				return -1;
+			}
+			break;
+		case 8:
+			if (writeListInFile(&head) < 0) {
+				return -1;
+			}
+			break;
+		case 9:
+			if (readListFromFile(&head) < 0) {
+				return -1;
+			}
+			break;
+		case 10:
 			isRunning = 0;
 			clearMemory(&head);
 			break;
@@ -86,7 +111,8 @@ int main()
 int addStart(Position prev) {
 	Position p = NULL;
 	p = createNode(p);
-	if (checkAllocation(p) < 0) {
+	if (p == NULL) {
+		printf("Greska u alokaciji memorije.\n");
 		return -1;
 	}
 
@@ -114,7 +140,8 @@ int printList(Position p) {
 int addEnd(Position p) {
 	Position q = NULL;
 	q = createNode(q);
-	if (checkAllocation(q) < 0) {
+	if (q == NULL) {
+		printf("Greska u alokaciji memorije.\n");
 		return -1;
 	}
 
@@ -129,14 +156,6 @@ int addEnd(Position p) {
 Position createNode(Position p) {
 	p = (Position)malloc(sizeof(Person));
 	return p;
-}
-
-int checkAllocation(Position p) {
-	if (p == NULL) {
-		printf("Greska u alokaciji memorije.\n");
-		return -1;
-	}
-	return 0;
 }
 
 int searchList(Position p) {
@@ -243,12 +262,14 @@ int clearMemory(Position head) {
 int deleteElement(Position p) {
 	char name[16], lastName[16];
 	int year;
-	Position q = p->next;
 
 	if (p->next == NULL) {
 		printf("Lista je prazna.\n");
 		return 0;
 	}
+
+	Position q = p->next;
+	
 
 	printf("Unesi ime, prezime i godinu osobe koju zelis ukloniti: ");	scanf(" %s %s %d", name, lastName, &year);
 	
@@ -266,6 +287,190 @@ int deleteElement(Position p) {
 
 	printf("Nije pronadena osoba %s %s %d.\n", name, lastName, year);
 
+
+	return 0;
+}
+
+int addElementAfter(Position p) {
+	char name[16], lastName[16];
+	int year;
+	int i = 0;
+	int personFound = 0;
+	Position q = NULL;
+	if (p->next == NULL) {
+		printf("Lista je prazna.\n");
+		return 0;
+	}
+
+	printf("Unesi ime osobe iza koje zelis ubaciti novu osobu: ");
+	scanf(" %s %s %d", name, lastName, &year);
+
+	while (p != NULL && personFound == 0) {
+		if (comparePersons(p, name, lastName, year) == 0) {
+			p = p->next;
+		}
+		else {
+			personFound = 1;
+		}
+	}
+
+	if (personFound == 1) {
+		q = createNode(q);
+		if (q == NULL) {
+			printf("Greska u alokaciji memorije.\n");
+			return -1;
+		}
+		printf("Unesi ime osobe: ");	scanf(" %s %s %d", q->firstName, q->lastName, &q->yearOfBirth);
+		q->next = p->next;
+		p->next = q;
+	}
+	else {
+		printf("%s %s %d nije pronaden u listi.\n", name, lastName, year);
+	}
+
+
+
+	return 0;
+}
+
+int comparePersons(Position p, char name[16], char lastName[16], int year) {
+	if (_strcmpi(p->firstName, name) == 0 && _strcmpi(p->lastName, lastName) == 0 && p->yearOfBirth == year) {
+		return 1;
+	}
+	return 0;
+}
+
+int addElementBefore(Position p) {
+	char name[16], lastName[16];
+	int year;
+	int personFound = 0;
+
+	if (p->next == NULL) {
+		printf("Lista je prazna.\n");
+		return 0;
+	}
+
+	printf("Unesi ime osobe ispred koje zelis ubaciti novu osobu: ");
+	scanf(" %s %s %d", name, lastName, &year);
+
+	while (p != NULL && personFound == 0) {
+		if (comparePersons(p->next, name, lastName, year) == 0) {
+			p = p->next;
+		}
+		else {
+			personFound = 1;
+		}
+	}
+
+	if (personFound == 1) {
+		if (addStart(p) < 0) {
+			return -1;
+		}
+	}
+	else {
+		printf("%s %s %d nije u listi.\n", name, lastName, year);
+	}
+
+
+
+
+
+	return 0;
+}
+
+int writeListInFile(Position p) {
+	char filename[16];
+	FILE* f = NULL;
+
+	if (p->next == NULL) {
+		printf("Lista je prazna.\n");
+		return 0;
+	}
+
+
+	printf("Unesi ime datoteke u koju zelis zapisati listu: ");
+	scanf(" %s", filename);
+
+	f = fopen(filename, "w");
+	if (f == NULL) {
+		printf("Greska pri otvaranju filea.\n");
+		return -1;
+	}
+
+	while (p->next != NULL) {
+		p = p->next;
+		fprintf(f, "%s %s %d\n", p->firstName, p->lastName, p->yearOfBirth);
+	}
+
+	printf("Lista je upisana u datoteku.\n");
+
+	
+	fclose(f);
+	return 0;
+}
+
+int readListFromFile(Position p) {
+	char filename[16];
+	FILE* f = NULL;
+	char name[16], lastName[16];
+	int year;
+
+	if (p->next != NULL) {
+		printf("Lista mora biti prazna!\n");
+		return 0;
+	}
+
+	printf("Unesi ime datoteke iz koje zelis ucitati listu: ");
+	scanf(" %s", filename);
+
+	f = fopen(filename, "r");
+	if (f == NULL) {
+		printf("Greska pri otvaranju filea.\n");
+		return -1;
+	}
+
+
+	while (!feof(f)) {
+		fscanf(f, " %s %s %d", name, lastName, &year);
+		if (addSorted(p, name, lastName, year) < 0) {
+			return -1;
+		}
+	}
+
+
+
+
+
+	fclose(f);
+	return 0;
+}
+
+int addSorted(Position p, char name[16], char lastName[16], int year) {
+	Position q = NULL;
+	q = createNode(q);
+	if (q == NULL) {
+		printf("Greska u alokaciji memorije.\n");
+		return -1;
+	}
+
+
+	if (p->next == NULL) {
+		strcpy(q->firstName, name);
+		strcpy(q->lastName, lastName);
+		q->yearOfBirth = year;
+		q->next = p->next;
+		p->next = q;
+	}
+	else {
+		while (p->next != NULL && _strcmpi(p->next->lastName, lastName) < 0) {
+			p = p->next;
+		}
+		strcpy(q->firstName, name);
+		strcpy(q->lastName, lastName);
+		q->yearOfBirth = year;
+		q->next = p->next;
+		p->next = q;
+	}
 
 	return 0;
 }
